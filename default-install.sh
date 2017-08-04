4f24207e68a6880fe8d3883ed962d6cfeb450869
Massimo Dong <dongmassimo@gmail.com>
2017-08-04 13:55:24 +0800

    install script

---
+++ default-install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if [ "$(id -u)" != "0" ];then
+	echo "This script must be run as root" 1>&2
+	exit 1
+fi
+
+JUDGERNAME="judger";
+JUDGERUID=1537;
+
+if id -u $JUDGERNAME > /dev/null;then
+	echo "User Exists";
+else
+	echo "Creating user and directory"
+	mkdir -p /home/$JUDGERNAME
+	mkdir -p /home/$JUDGERNAME/etc
+	mkdir -p /home/$JUDGERNAME/sim
+	cp ./doc/config.json.example /home/$JUDGERNAME/etc/config.json
+
+	useradd -u $JUDGERUID -d /home/$JUDGERNAME -s /bin/bash $JUDGERNAME
+
+	chown -R $JUDGERNAME.$JUDGERNAME /home/$JUDERNAME
+fi
+
+./autogen.sh
+make
+make install
