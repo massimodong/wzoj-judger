2d1a824ccbb2a050b46f77b8caade3131be99c49
Massimo Dong <dongmassimo@gmail.com>
2020-03-25 20:20:46 +0800

    okcalls

--- src/okcalls64.h
+++ src/okcalls64.h
@@ -17,18 +17,18 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-int LANG_CV[256] = {0,1,2,4,5,9,11,12,21,59,63,89,158,231,240, 8,
+int LANG_CV[256] = {0,1,2,4,5,9,10,11,12,21,59,63,89,158,231,240, 8,
 	 SYS_time, SYS_read, SYS_uname, SYS_write, SYS_open,
 		SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_munmap, SYS_mprotect,
 		SYS_mmap, SYS_fstat, SYS_set_thread_area, 252, SYS_arch_prctl, 231, 0 };
 
-int LANG_PV[256] = {0,1,2,4,9,11,13,16,59,89,97,201,231,
+int LANG_PV[256] = {0,1,2,4,9,10,11,13,16,59,89,97,201,231,
 	SYS_open, SYS_set_thread_area, SYS_brk, SYS_read,
 		SYS_uname, SYS_write, SYS_execve, SYS_ioctl, SYS_readlink, SYS_mmap,
 		SYS_rt_sigaction, SYS_getrlimit, 252, 191, 158, 231, SYS_close,
 		SYS_exit_group, SYS_munmap, SYS_time, 4, 0 };
 
-int LANG_YV[256] = { 0,1,2,3,4,5,6,8,9,10,11,12,13,14,16,21,32,39,41,42,59,
+int LANG_YV[256] = { 0,1,2,3,4,5,6,8,9,10,11,12,13,14,16,17,21,32,39,41,42,59,
 	72,78,79,89,97,99,102,104,107,108,131,158,202,218,231,257,273
 		, 146, SYS_mremap, 158, 117, 60, 102, 191, 217, 302, 318,
 		SYS_access, SYS_arch_prctl, SYS_brk, SYS_close, SYS_execve,
