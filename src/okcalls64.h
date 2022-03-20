f5e9b8666518df2a33a4284ecb85cfcbfc977c68
Massimo Dong <dongmassimo@gmail.com>
2022-03-20 21:06:53 +0800

    update okcalls64

--- src/okcalls64.h
+++ src/okcalls64.h
@@ -17,7 +17,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-int LANG_CV[256] = {0,1,2,4,5,9,10,11,12,20,21,59,63,89,158,231,240, 262, 8,
+int LANG_CV[256] = {0,1,2,4,5,9,10,11,12,20,21,59,63,89,158,218,231,240, 262,273,302,318,334, 8,
 	 SYS_time, SYS_read, SYS_uname, SYS_write, SYS_open,
 		SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_munmap, SYS_mprotect,
 		SYS_mmap, SYS_fstat, SYS_set_thread_area, 252, SYS_arch_prctl, 231, 0 };
